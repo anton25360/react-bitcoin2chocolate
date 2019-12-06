913e87b726c1a182ce7167f86138b6c5165c0f71
Anton Webb <anton25360@gmail.com>
2019-12-06 09:49:13 +0000

    Init build folder

---
+++ build/precache-manifest.49c1ce7df92a7b326896ea3a7fd4282a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8a04d29c67a768e028ec82342eb01f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48e60243324d9c749ec9",
+    "url": "/static/css/main.f7a53bb2.chunk.css"
+  },
+  {
+    "revision": "92aca6ee4cad6731dba2",
+    "url": "/static/js/2.4b991425.chunk.js"
+  },
+  {
+    "revision": "48e60243324d9c749ec9",
+    "url": "/static/js/main.0fb57d6b.chunk.js"
+  },
+  {
+    "revision": "5c60c91714d1f1960d53",
+    "url": "/static/js/runtime-main.2bcf60b6.js"
+  }
+]);
